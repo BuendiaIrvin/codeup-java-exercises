924cb106d726342706eca739baf451e11fb3b8e5
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-24 14:06:11 -0500

    completed syntax java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,45 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.print("my name is Jeff.");
+        System.out.println("my name is Jeff.");
+
+
+//
+//        int myFavoriteNumber = 9;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Hello from Java!";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+
+//        int x = 4;
+//        x = x + 5;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//         y = y * x;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        x /= y;
+//        y -= x;
+
     }
 }
+
