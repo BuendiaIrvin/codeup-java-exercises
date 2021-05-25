35c1d0138c01f12cf86b3d540b7fb9fc3f34533c
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-25 15:12:14 -0500

    completed password instead of fizzbuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //         1.a while
@@ -51,6 +53,21 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //        }
 
+        {
+            final String PASSWORD = "Test";
+            Scanner sc = new Scanner(System.in);
+            int attempts = 3;
+            String password = "";
+            while (attempts-- > 0 && !PASSWORD.equals(password)) //compares and then decrements
+            {
+                System.out.print("Enter your password: ");
+                password = sc.nextLine();
+                if (password.equals(PASSWORD))
+                    System.out.println("Welcome");
+                else
+                    System.out.println("Incorrect. Number of attempts remaining: " + attempts);
+            }
 
+        }
     }
 }
