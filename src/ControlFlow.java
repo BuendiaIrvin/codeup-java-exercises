b7cd6e4777c4b907307dfe2be880c15349f1535f
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-25 14:18:32 -0500

    completed exercise 1.A

---
+++ src/ControlFlow.java
@@ -0,0 +1,71 @@
+public class ControlFlow {
+    public static void main(String[] args) {
+//        int x = 5;
+//        if (x == 5) {
+//            System.out.println("if you are reading this, it means x is 5");
+//        }
+//        System.out.println("this says nothing about x, because it runs regardless of what x is");
+
+
+//        int y = 3;
+//        if (y == 2) {
+//            System.out.println("ive carried out a conditional test and i can confirm that the statement 'y==2' is true");
+//        } else {
+//            System.out.println("ive carried out a conditional test to see if 'y==2' and its not. it is false");
+//        }
+
+//        short y = 32000;
+//        if (y < 0) {
+//            System.out.println("The variable y contains a negative number value");
+//        } else if (y >=0 && y < 30000){
+//            System.out.println("The variable y contains a positive number value");
+//        } else {
+//            System.out.println("The variable y contains a numaric value that is close to the max value for sort type variable, which is " + Short.MAX_VALUE);
+//        }
+//
+//        String customerChoice;
+//        customerChoice = "Chocolate";
+//
+//        switch (customerChoice){
+//            case "vanilla":
+//                System.out.println("One vanilla coming right up");
+//                break;
+//            case "chocolate":
+//                System.out.println("One chocolate coming right up");
+//                break;
+//            case "strawberry":
+//                System.out.println("One strawberry coming right up");
+//                break;
+//            default:
+//                System.out.println("We do not have that");
+//                break;
+//        }
+
+//        int c = 0, d = 100, e = 50;
+//
+//        if (c == 1 && e++ < 100) {
+//        // do anything
+//        }
+//        System.out.println("e = " + e);
+//
+//        if (c == 0 && e++ < 100) {
+//            //do anything
+//            d = 150;
+//        }
+//        System.out.println("e = " + e);
+//
+//        // non short circut operator
+//        if (c == 1 & e++ < 100) {
+//            //anything goes here
+//        }
+//        System.out.println("e = " + e);
+
+        for (int i = 0; i < 10; i++){
+            System.out.println("now im in the loop. i has not incremented yet and its value is " + i);
+        }
+
+
+
+
+    }
+}
