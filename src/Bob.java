0cf38d017a17eed6ebf6fa4e10725f47e82e6cdb
Irvin Buendia <irvin.buendia1@gmail.com>
2021-05-27 09:36:05 -0500

    Bob excercise with walkthrough

--- src/Bob.java
+++ src/Bob.java
@@ -6,16 +6,45 @@ import java.util.Scanner;
 public class Bob {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-
-        System.out.println("Would you like to have a conversation with Bob?");
-        String userInput = scanner.nextLine();
-        if (userInput.endsWith("?")) {
-            System.out.println("Sure?");
-        } else if (userInput.endsWith("!")) {
-            System.out.println("Whoa, chill out!");
-        } else if (userInput.equals("")) {
-            System.out.println("Fine. Be that way!");
+        String response = "";
+        while (!response.equalsIgnoreCase("QUIT")) {
+            System.out.println("BOB: What do you want..");
+            String userInput = scanner.nextLine();
+            if (userInput.endsWith("?")) {
+                System.out.println("Sure?");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Whoa, chill out!");
+            } else if (userInput.equals("")) {
+                System.out.println("Fine. Be that way!");
+            } else {
+                System.out.println("Whatever");
+            }
         }
     }
 }
+
+//davids bob
+
+//public class Bob {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Hey, my name's Bob... My mom said you wanted to talk to me, or something... Whatever...");
+//        String response = "";
+//        while(!response.equalsIgnoreCase("QUIT")) {
+//            System.out.println("What do you want?");
+//            response = sc.nextLine();
+//            if (response.isEmpty()) {
+//                System.out.println("Fine. Be that way!");
+//            } else if (response.endsWith("?")) {
+//                System.out.println("Sure.");
+//            } else if (response.endsWith("!")) {
+//                System.out.println("Whoa, chill out!");
+//            } else if (response.equalsIgnoreCase("quit")){
+//                System.out.println("Finally...");
+//            } else {
+//                System.out.println("Whatever.");
+//            }
+//        }
+//        System.out.println("I'm out of here...");
+//    }
+//}
---
+++ src/MethodsLec.java
@@ -0,0 +1,5 @@
+public class MethodsLec {
+    public static void main(String[] args) {
+
+    }
+}
