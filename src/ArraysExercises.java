749992eaedf2efd25fb74425244beb49c3c4a331
Irvin Buendia <irvin.buendia1@gmail.com>
2021-06-03 11:30:33 -0500

    completed first part of arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,8 +1,20 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
     public static void main(String[] args) {
-//        int[] numbers = {1, 2, 3, 4, 5};
-//        System.out.println(numbers);
+    String[] Person = new String[3];
+        Person[0] = "Greg";
+        Person[1] = "Jim";
+        Person[2] = "Geoff";
+        System.out.println(Arrays.toString(Person));
+        int n = Person.length;
 
+
+        // need to make it a static method
+        String[] morePersons = Arrays.copyOf(Person,  n + 1);
+        int nl = morePersons.length - 1;
+        morePersons[nl] = "Greg";
+        System.out.println(Arrays.toString(morePersons));
 
     }
 }
